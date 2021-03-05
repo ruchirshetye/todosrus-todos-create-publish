ac727ed960eb72a0b655b26f641fe55f37263a8e
ruchirshetye <32875336+ruchirshetye@users.noreply.github.com>
2021-03-05 14:29:16 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,14 +37,16 @@ Resources:
         CurrentVersion: "$DEVELOPMENT_ALIAS_VERSION"
         TargetVersion: "$LATEST_VERSION"
 EOM
-aws s3 cp \
-    $DEPLOY_APPSPEC_FILE \
-    s3://$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE
+
+#aws s3 cp \
+#    $DEPLOY_APPSPEC_FILE \
+#    s3://$DEPLOY_BUCKET_NAME/$DEPLOY_APPSPEC_FILE
+ls
 
 # CREATE DEPLOYMENT
-REVISION=revisionType=S3,s3Location={bucket=$DEPLOY_BUCKET_NAME,key=$DEPLOY_APPSPEC_FILE,bundleType=YAML}
-aws deploy create-deployment \
-  --application-name $DEPLOY_APPLICATION_NAME \
-  --deployment-group-name $DEPLOY_DEPLOYMENT_GROUP_NAME \
-  --deployment-config-name CodeDeployDefault.LambdaAllAtOnce \
+#REVISION=revisionType=S3,s3Location={bucket=$DEPLOY_BUCKET_NAME,key=$DEPLOY_APPSPEC_FILE,bundleType=YAML}
+#aws deploy create-deployment \
+#  --application-name $DEPLOY_APPLICATION_NAME \
+ # --deployment-group-name $DEPLOY_DEPLOYMENT_GROUP_NAME \
+ # --deployment-config-name CodeDeployDefault.LambdaAllAtOnce \
   --revision $REVISION
